e70eacbe8cb9b4240745a68ab152ef2b9ebfdc1a
AmitMY <amitmoryossef@gmail.com>
2025-09-03 09:51:37 +0200

    feat(benchmarks): add processor and tokenizer performance benchmarks

---
+++ examples/benchmark_processor.py
@@ -0,0 +1,26 @@
+from datasets import load_dataset
+from tqdm import tqdm
+from transformers import AutoImageProcessor
+from trl import pack_dataset
+
+from image_latent_transformer.processor import TextImageProcessor
+from image_latent_transformer.tokenizer.utf8 import UTF8Tokenizer
+
+dataset = load_dataset("Helsinki-NLP/opus-100", "en-he", split="train")
+
+processor = TextImageProcessor(
+    tokenizer=UTF8Tokenizer(),
+    image_processor=AutoImageProcessor.from_pretrained("WinKawaks/vit-tiny-patch16-224", use_fast=True),
+)
+
+# Convert dataset to text
+dataset = dataset.map(
+    lambda batch: {"text": f"<en>\x0E{batch['translation']['en']}\x0F<he> {batch['translation']['he']}"},
+    remove_columns=["translation"])
+dataset = processor.pretokenize_dataset(dataset)
+
+dataset = pack_dataset(dataset, seq_length=128)
+dataset = dataset.with_transform(processor)
+
+for _ in tqdm(dataset):
+    pass
